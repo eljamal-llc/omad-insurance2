f52cecadff566aafbe0ebc607ea4dfa337a77de4
Nizomiddin Zaripov <96042532+davidkhan4747@users.noreply.github.com>
2021-12-20 10:54:27 +0500

    single page

--- components/mtpl-insurance/index.tsx
+++ components/mtpl-insurance/index.tsx
@@ -1,1 +1,2 @@
 export { default as MtplInsuranceHome } from './mtpl-insurance-home/mtpl-insurance-home';
+export {default as MtplAdventages } from './mtpl-advantages/mtpl-advantages'
---
+++ components/mtpl-insurance/mtpl-advantages/mtpl-advantages.tsx
@@ -0,0 +1,46 @@
+import {MtplAdventagesProps} from './mtpl-advantages.t'
+import { FC } from "react";
+import { GWrapper } from '../../../styles/global-styles.e';
+
+import {   
+     MtplAdventagesHeading , 
+     MtplAdventagesBlock,
+     MtplAdventagesCard,
+     MtplAdventagesCardHeading,
+     MtplAdventagesCardText
+} from './mtpl-advantages.e';
+const MtplAdventages :FC<MtplAdventagesProps> =  (props : MtplAdventagesProps) =>{
+    return (
+        // <GWrapper>
+        <>
+        <MtplAdventagesHeading>Преимущества страхования ОСАГО онлайн</MtplAdventagesHeading>
+            <MtplAdventagesBlock>
+                <MtplAdventagesCard>
+                    <MtplAdventagesCardHeading>Максимально онлайн</MtplAdventagesCardHeading>
+                    <MtplAdventagesCardText>В OMAD основные процессы — от оформления до урегулирования — онлайн. Быстро и удобно</MtplAdventagesCardText>
+                </MtplAdventagesCard>
+                <MtplAdventagesCard>
+                    <MtplAdventagesCardHeading>Максимально онлайн</MtplAdventagesCardHeading>
+                    <MtplAdventagesCardText>В OMAD основные процессы — от оформления до урегулирования — онлайн. Быстро и удобно</MtplAdventagesCardText>
+                </MtplAdventagesCard>
+                <MtplAdventagesCard>
+                    <MtplAdventagesCardHeading>Максимально онлайн</MtplAdventagesCardHeading>
+                    <MtplAdventagesCardText>В OMAD основные процессы — от оформления до урегулирования — онлайн. Быстро и удобно</MtplAdventagesCardText>
+                </MtplAdventagesCard>
+                <MtplAdventagesCard>
+                    <MtplAdventagesCardHeading>Максимально онлайн</MtplAdventagesCardHeading>
+                    <MtplAdventagesCardText>В OMAD основные процессы — от оформления до урегулирования — онлайн. Быстро и удобно</MtplAdventagesCardText>
+                </MtplAdventagesCard>
+
+            </MtplAdventagesBlock>
+        </>
+            
+
+
+
+        // </GWrapper>
+
+
+    )
+}
+export default MtplAdventages
