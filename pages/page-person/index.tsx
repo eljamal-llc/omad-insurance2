9cd4d9626467c153494919909c466b915945718a
moons1997 <50578926+moons1997@users.noreply.github.com>
2022-01-13 14:45:29 +0500

    two branch marge

--- pages/page-person/index.tsx
+++ pages/page-person/index.tsx
@@ -72,14 +72,24 @@ const Partner: FC<NextPage> = () => {
     }
   };
   const { t } = useTranslation();
-
+  // test
   return (
     <>
       {!loading ? (
         <Layout title={t("common:Property_insurance")}>
           <Navbar />
-        <BreadcrumbsBlock url2={ 'polit_market' ? 'insurance-case?id=polit_market' :'insurance-case?id=insurance-case' } url3={''} link1="Главная " link2={'polit_market' ? "МАГАЗИН ПОЛИС " : 'СТРАХОВОЙ СЛУЧАЙ'} link3="" />
-          
+          <BreadcrumbsBlock
+            url2={
+              "polit_market"
+                ? "insurance-case?id=polit_market"
+                : "insurance-case?id=insurance-case"
+            }
+            url3={""}
+            link1="Главная "
+            link2={"polit_market" ? "МАГАЗИН ПОЛИС " : "СТРАХОВОЙ СЛУЧАЙ"}
+            link3=""
+          />
+
           <Hero data={sliders} />
           <WrapperTitle
             title={t("common:Services")}
