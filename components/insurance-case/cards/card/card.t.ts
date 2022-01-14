c89603912d4b719ba75f35d62d8a769f6cdae6cc
moons1997 <50578926+moons1997@users.noreply.github.com>
2022-01-14 16:28:36 +0500

    shop

--- components/insurance-case/cards/card/card.t.ts
+++ components/insurance-case/cards/card/card.t.ts
@@ -1,3 +1,4 @@
 export interface CardProps {
   id?: any;
+  item?: any;
 }
--- components/insurance-case/cards/cards.tsx
+++ components/insurance-case/cards/cards.tsx
@@ -1,4 +1,4 @@
-import { FC, Key} from "react";
+import { FC, Key } from "react";
 
 import { CardsProps } from "./cards.t";
 import { Wrapper } from "./cards.e";
@@ -9,19 +9,9 @@ const CardsCase: FC<CardsProps> = ({ id, data }) => {
   return (
     <Wrapper>
       <CardsRow>
-        {id == 17 ? (
-          <>
-            {data?.map((item: any, idx: Key | null | undefined) => (
-              <Card id={id} key={idx} />
-            ))}
-          </>
-        ) : (
-          <>
-            {data?.map((item: any, idx: Key | null | undefined) => (
-              <Card id={id} key={idx} />
-            ))}
-          </>
-        )}
+        {data?.map((item: any, idx: Key | null | undefined) => (
+          <Card id={id} key={idx} item={item} />
+        ))}
       </CardsRow>
     </Wrapper>
   );
