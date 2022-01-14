4ffeb5fc6eb8bbef142c9056ea94ef1bc3374c04
Nizomiddin Zaripov <96042532+davidkhan4747@users.noreply.github.com>
2022-01-14 17:06:00 +0500

    styles

--- components/about/wrapper-category/wrapper-category.e.tsx
+++ components/about/wrapper-category/wrapper-category.e.tsx
@@ -15,6 +15,9 @@ export const WrapperCategory = styled.div`
   font-family: Roboto;
   font-style: normal;
   font-weight: normal;
+  height: 109px;
+    display: flex;
+    align-items: center;
   cursor: pointer;
   font-size: 24px;
   line-height: 28px;
@@ -29,7 +32,7 @@ export const WrapperCategory = styled.div`
 export const WrapperTags = styled.div`
   padding: 40px 50px;
   @media (max-width: 1800px) {
-    padding: 3% 5%;
+    padding: 30px 5%;
   }
 `;
 export const TagsTitle = styled.div`
@@ -41,16 +44,26 @@ export const TagsTitle = styled.div`
   color: #ffffff;
   background-color: #f0803d;
   padding: 10px 20px;
-  margin-bottom: 20px;
+  /* margin-bottom: 20px; */
   border: 2px solid #f0803d;
   display: inline-block;
 `;
 export const TagsRow = styled.div`
   display: flex;
+  width: 1200px;
+  flex-wrap: wrap;
 `;
 export const Tag = styled.div`
   display: inline-block;
   margin-right: 20px;
+  margin-top:15px ;
+  border: 2px solid #f0803d;
+  color: #ffffff;
+  cursor: pointer;
+  padding: 10px 20px;
+  &:hover {
+    background-color: #f0803d;
+  }
   a {
     font-family: Roboto;
     font-style: normal;
