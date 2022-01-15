5aa13ff204910ef3fbddfca2559d29a10aa82079
moons1997 <50578926+moons1997@users.noreply.github.com>
2022-01-15 19:55:27 +0500

    contact api

--- components/contancts/contacts-home/contacts-home.tsx
+++ components/contancts/contacts-home/contacts-home.tsx
@@ -15,27 +15,38 @@ import { GWrapper } from "../../../styles/global-styles.e";
 import ContImg from "../../../assets/images/contacts/contact.png";
 import BreadcrumbsBlock from "../../common/bread-crumbs/Breadcrumbs";
 import { useTranslation } from "react-i18next";
-const ContactsHome: FC<ContactsHomeProps> = (props: ContactsHomeProps) => {
-  const {t} = useTranslation()
+const ContactsHome: FC<ContactsHomeProps> = ({ data }) => {
+  const { t } = useTranslation();
   return (
-    <ContactsHomeBG>
-      <ContactsHomeBlock>
-        <ContactsBlock>
-          <BreadcrumbsBlock link1="Главная" url3={''} url2={'/contacts'} link2="Контакты" link3="" />
-          <ContactsBlockHeading> {t('common:Contacts')}</ContactsBlockHeading>
-          <ContactsBlockSlogan>«OMAD SUG’URTA»</ContactsBlockSlogan>
-          <ContactsBlockLokations>{t('common:Tashkent_city')}</ContactsBlockLokations>
-          <ContactsBlockLokations>{t('common:Shaykhantakhur ')}</ContactsBlockLokations>
-          <ContactsBlockLokations>{t('common:Lilithugon')}</ContactsBlockLokations>
-          <ContactsBlockLokations>
-            {t('common:tel')} : +998 (71) 249-02-77 e-mail: omad.insurance.g@mail.ru
-          </ContactsBlockLokations>
-        </ContactsBlock>
-        <ConImg>
-          <Image src={ContImg} alt="test" />
-        </ConImg>
-      </ContactsHomeBlock>
-    </ContactsHomeBG>
+    data && (
+      <ContactsHomeBG>
+        <ContactsHomeBlock>
+          <ContactsBlock>
+            <BreadcrumbsBlock
+              link1="Главная"
+              url3={""}
+              url2={"/contacts"}
+              link2="Контакты"
+              link3=""
+            />
+            <ContactsBlockHeading> {data.title}</ContactsBlockHeading>
+            <ContactsBlockSlogan>{data.company}</ContactsBlockSlogan>
+            <ContactsBlockLokations>
+              address: {data.address}
+            </ContactsBlockLokations>
+            <ContactsBlockLokations>
+              {t("common:tel")} : {data.tel} <br />
+              e-mail: {data.email}
+            </ContactsBlockLokations>
+          </ContactsBlock>
+          <ConImg>
+            {/* <Image src={ContImg} alt="test" />
+             */}
+            <img src={data.image} alt={data.company} />
+          </ConImg>
+        </ContactsHomeBlock>
+      </ContactsHomeBG>
+    )
   );
 };
 export default ContactsHome;
--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import type { NextPage } from "next";
 import {
   Layout,
@@ -6,31 +6,48 @@ import {
   Footer,
   ContactsHome,
   News,
-  ContactsCards
+  ContactsCards,
 } from "../../components";
-import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
-import {useTranslation} from 'next-i18next'
- 
-export async  function getStaticProps({locale}:{locale : string} ) {
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { api } from "../../services/api";
+import { INewsData } from "../../components/common/news/news.t";
+
+export async function getStaticProps({ locale }: { locale: string }) {
   return {
-    props:{
-     ...(await serverSideTranslations(locale, [
-        'common'
-      ]))
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
     },
   };
 }
 export interface PartnerProps {}
 
 const Contacts: FC<NextPage> = () => {
+  const [contact, setContact] = useState();
+  const [news, setNews] = useState<INewsData[] | []>([]);
+  useEffect(() => {
+    api.get("contacts").then((res) => {
+      // console.log("---------", res);
+      setContact(res.data);
+    });
+    api.get("news").then((res) => {
+      setNews(res.data.data);
+    });
+  }, []);
   return (
     <Layout title="КОНТАКТЫ">
       <Navbar />
-      <ContactsHome/>
-      <ContactsCards/>
-      <News/>
-      <Footer/>
+      {contact && (
+        <>
+          {" "}
+          {/* @ts-ignore */}
+          <ContactsHome data={contact.main_content} /> {/* @ts-ignore */}
+          <ContactsCards data={contact.filials} />
+        </>
+      )}
 
+      <News data={news} />
+      <Footer />
     </Layout>
   );
 };
