3164643bde18f22a42cdac8c8a236ad191feb483
Nizomiddin Zaripov <96042532+davidkhan4747@users.noreply.github.com>
2022-01-17 11:59:48 +0500

    Update auth-page.e.tsx

--- components/auth/auth-page/auth-page.e.tsx
+++ components/auth/auth-page/auth-page.e.tsx
@@ -27,7 +27,7 @@ export const AuthBlock = styled.div`
     font-weight: normal;
     font-size: 24px;
     line-height: 28px;
-    color: #ffffff;
+    color: #ffffff !important;
     padding: 25px 0;
     transition: all 0.4s ease-out;
     text-transform: capitalize;
