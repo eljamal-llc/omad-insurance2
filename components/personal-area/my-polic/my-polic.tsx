35a9950c3431d85067bc8f01783ce227288b733f
Nizomiddin Zaripov <96042532+davidkhan4747@users.noreply.github.com>
2022-01-18 12:36:17 +0500

    commit

--- components/personal-area/my-polic/my-polic.tsx
+++ components/personal-area/my-polic/my-polic.tsx
@@ -26,28 +26,28 @@ const MyPolic: FC<MyPolicProps> = () => {
                         <PersonalAreaBodyCard>
                             <PersonalAreaBodyCardHeading>{t('common:My_details')}</PersonalAreaBodyCardHeading>
                             <PersonalAreaBodyCardText>{t('common:info_details_personal_area')}</PersonalAreaBodyCardText>
-                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:More_details')}</a></Link></CardButton></ButtonCard>
+                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:dow')}</a></Link></CardButton></ButtonCard>
                         </PersonalAreaBodyCard>
                         <PersonalAreaBodyCard>
                             <PersonalAreaBodyCardHeading>{t('common:My_insurance_policies')}</PersonalAreaBodyCardHeading>
                             <PersonalAreaBodyCardText>{t('common:infp_ins_polic_List')}</PersonalAreaBodyCardText>
-                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:More_details')}</a></Link></CardButton></ButtonCard>
+                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:dow')}</a></Link></CardButton></ButtonCard>
                         </PersonalAreaBodyCard>
                         <PersonalAreaBodyCard>
                             <PersonalAreaBodyCardHeading>{t('common:buy_online')}</PersonalAreaBodyCardHeading>
                             <PersonalAreaBodyCardText>{t('common:buy_online_info')}</PersonalAreaBodyCardText>
-                            <ButtonCard><CardButton><Link href={'/personal-area/buy'}><a>{t('common:More_details')}</a></Link></CardButton></ButtonCard>
+                            <ButtonCard><CardButton><Link href={'/personal-area/buy'}><a>{t('common:dow')}</a></Link></CardButton></ButtonCard>
                         </PersonalAreaBodyCard>
                         <PersonalAreaBodyCard>
                             <PersonalAreaBodyCardHeading>{t('common:renew_policy')}</PersonalAreaBodyCardHeading>
                             <PersonalAreaBodyCardText>{t('common:renew_policy_INFO')}</PersonalAreaBodyCardText>
                             <PersonalAreaBodyCardText>{t('common:renew_policy_INFO22')}</PersonalAreaBodyCardText>
-                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:More_details')}</a></Link></CardButton></ButtonCard>
+                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:dow')}</a></Link></CardButton></ButtonCard>
                         </PersonalAreaBodyCard>
                         <PersonalAreaBodyCard>
                             <PersonalAreaBodyCardHeading>{t('common:settings')}</PersonalAreaBodyCardHeading>
                             <PersonalAreaBodyCardText>{t('common:settings_info  ')}</PersonalAreaBodyCardText>
-                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:More_details')}</a></Link></CardButton></ButtonCard>
+                            <ButtonCard><CardButton><Link href={'/personal-area'}><a>{t('common:dow')}</a></Link></CardButton></ButtonCard>
                         </PersonalAreaBodyCard>
                 </PersonalAreaBodyCardBody>
             </GWrapper>
