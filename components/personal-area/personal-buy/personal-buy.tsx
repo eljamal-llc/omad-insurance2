ececbde4b12cc328f5e0123b303a8b28dd448eba
moons1997 <50578926+moons1997@users.noreply.github.com>
2022-01-21 19:20:18 +0500

    contact

--- components/personal-area/personal-buy/personal-buy.tsx
+++ components/personal-area/personal-buy/personal-buy.tsx
@@ -1,25 +1,30 @@
-import { FC, useState } from "react";
+import { FC, useState, useContext, forwardRef } from "react";
 import NextLink from "next/link";
-import {PersonalBuyProps} from './personal-buy.t';
+import { PersonalBuyProps } from "./personal-buy.t";
+import Router from "next/router";
 import {
-        BodyForm,
-        FormBody,
-        FormHeading,
-        PageForm,
-        UserInfoInput,
-        UserNumber,
-        UserEmail,
-        ButtonAdd,
-        FormMessage,
-        ButtonBlock
-        
-        } from './pesonal-buy.e'
-import { makeStyles } from '@mui/styles';
-import { FormControl, Input, InputLabel, TextField } from '@mui/material';
+  BodyForm,
+  FormBody,
+  FormHeading,
+  PageForm,
+  UserInfoInput,
+  UserNumber,
+  UserEmail,
+  ButtonAdd,
+  FormMessage,
+  ButtonBlock,
+} from "./pesonal-buy.e";
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import { makeStyles } from "@mui/styles";
+import { FormControl, Input, InputLabel, TextField } from "@mui/material";
 import { CardButton } from "../../yur-face-page/CardBlock/CardBlock.e";
 import { useTranslation } from "react-i18next";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import styled from "styled-components";
+import { AuthContext } from "../../../context/AuthContext";
+import { api } from "../../../services/api";
 // const UserInfoInput = styled(TextField)({
 //   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
 //   border: 0,
@@ -29,90 +34,179 @@ import styled from "styled-components";
 //   height: 48,
 //   padding: '0 30px',
 // });
+
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 const BuyPolis: FC<PersonalBuyProps> = () => {
-    const {t} = useTranslation()
-    const [add , isAdd] = useState('')
-    const [addButt , setAddBut] = useState('yes')
-    const AddHand = () =>{
-        isAdd('addet')
-        setAddBut('')
+  const { t } = useTranslation();
+  const [add, isAdd] = useState("");
+  const [addButt, setAddBut] = useState("yes");
+  const [contact, setContact] = useState<any>({});
+  const AddHand = () => {
+    isAdd("addet");
+    setAddBut("");
+  };
+  const RemoveBtn = () => {
+    isAdd("");
+    setAddBut("yes");
+  };
+  const {
+    signIn,
+    registration,
+    setOpen,
+    open,
+    alert,
+    setAlert,
+    errorMsg,
+    setErrorMsg,
+  } = useContext(AuthContext);
+  //   const router = useRouter();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    api
+      .post("/cabinet/contact-us", contact)
+      .then((res) => {
+        console.log("object ==>", res);
+        setOpen(true);
+        setContact({});
+        Router.push("/personal-area/buy");
+      })
+      .catch((err) => {
+        // console.log("object ==>", err.response.data);
+        setAlert(true);
+        // console.log(err.response.data);
+        setErrorMsg({
+          type: "contact",
+          message: err.response.data,
+        });
+      });
+  };
+  const handleChange = (event: any, field: any) => {
+    setContact((prevState: any) => ({
+      ...prevState,
+      [field]: event.target.value,
+    }));
+  };
+  //@ts-ignore
+  const [message, setMessage] = useState(t("common:Message"));
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
     }
-    const RemoveBtn = () =>{
-        isAdd('')
-        setAddBut('yes')
-    }
-    //@ts-ignore
-    const  [message , setMessage] = useState( t('common:Message'))
-    return (
-        <>
-        <BodyForm>
-            <FormBody>
-                <FormHeading>{t('common:contact_us')}</FormHeading>
-                <FormControl>
-                <PageForm>
-                <UserInfoInput
-                        id="demo-helper-text-misaligned"
-                        placeholder="Иванов Иван Иванович"
-                        label={t('common:name_data')}
-                        /><br/>
-                    <UserNumber
-                        className='myInput'
-                        placeholder="998 90 989-89-89"
-                        id="demo-helper-text-misaligned"
-                        label={t('common:Phone_number')}
-                        
-                        />
-                    <UserEmail
-                        className='myInput'
-                        placeholder="status585@mail.ru"
-                        id="demo-helper-text-misaligned"
-                        label="Email"
-                        
-                        
-                        />
-                        {add == 'addet' ? (
-                            <>
-                             <UserNumber
-                            className='myInput'
-                            id="demo-helper-text-misaligned"
-                            label={t('common:Phone_number')}
-                            
-                            />
-                        <UserEmail
-                            className='myInput'
-                            // helperText="Please enter your email"
-                            id="demo-helper-text-misaligned"
-                            label="Email"
-                            
-                            
-                            />
-                            </>
-                           
-                        ): ('')}
-                    <div>
-                         { addButt == 'yes' ? (
-                             
-                            <ButtonAdd onClick={AddHand}> + {t('common:Add')}</ButtonAdd>
 
-                         ): <ButtonAdd onClick={RemoveBtn}>Убрать</ButtonAdd>} 
-                    </div>
-                    {/* @ts-ignore */}
-                    <FormMessage onClick={()=>{ setMessage(t(''))}}  placeholder={message}/>
-                    <ButtonBlock>
-                        <CardButton>{t('common:send')}</CardButton>
-                    </ButtonBlock>
-                
-                </PageForm>
-                </FormControl>
-            </FormBody>
-        </BodyForm>
-     
+    setOpen(false);
+    setAlert(false);
+    setErrorMsg(null);
+  };
+  return (
+    <>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
 
+      <Snackbar open={alert} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          {/* @ts-ignore */}
+          {/* {errorMsg && errorMsg.type == "polis" && errorMsg.message} */}
 
-        </>
-
-    )
-
-}
+          {errorMsg &&
+            errorMsg.type == "polis" &&
+            // @ts-ignore
+            errorMsg.message.map((item, idx) => {
+              <div key={idx}>{item}</div>;
+            })}
+        </Alert>
+      </Snackbar>
+      <BodyForm>
+        <FormBody>
+          <FormHeading>{t("common:contact_us")}</FormHeading>
+          <FormControl>
+            <PageForm onClick={handleSubmit}>
+              <UserInfoInput
+                id="demo-helper-text-misaligned"
+                placeholder="Иванов Иван Иванович"
+                label={t("common:name_data")}
+                onChange={(e) => handleChange(e, "full_name")}
+                name="full_name"
+                value={contact.full_name}
+                required
+              />
+              <br />
+              <UserNumber
+                className="myInput"
+                placeholder="998 90 989-89-89"
+                id="demo-helper-text-misaligned"
+                label={t("common:Phone_number")}
+                onChange={(e) => handleChange(e, "phonenumber1")}
+                name="phonenumber1"
+                value={contact.phonenumber1}
+                required
+              />
+              <UserEmail
+                className="myInput"
+                placeholder="status585@mail.ru"
+                id="demo-helper-text-misaligned"
+                label="Email"
+                onChange={(e) => handleChange(e, "email1")}
+                name="email1"
+                value={contact.email1}
+                required
+              />
+              {add == "addet" ? (
+                <>
+                  <UserNumber
+                    className="myInput"
+                    id="demo-helper-text-misaligned"
+                    label={t("common:Phone_number")}
+                    onChange={(e) => handleChange(e, "phonenumber2")}
+                    name="phonenumber2"
+                    value={contact.phonenumber2}
+                  />
+                  <UserEmail
+                    className="myInput"
+                    // helperText="Please enter your email"
+                    id="demo-helper-text-misaligned"
+                    label="Email"
+                    onChange={(e) => handleChange(e, "email2")}
+                    name="email2"
+                    value={contact.email2}
+                  />
+                </>
+              ) : (
+                ""
+              )}
+              <div>
+                {addButt == "yes" ? (
+                  <ButtonAdd onClick={AddHand}> + {t("common:Add")}</ButtonAdd>
+                ) : (
+                  <ButtonAdd onClick={RemoveBtn}>Убрать</ButtonAdd>
+                )}
+              </div>
+              {/* @ts-ignore */}
+              <FormMessage
+                onChange={(e) => handleChange(e, "text")}
+                name="text"
+                value={contact.text}
+                // placeholder={message}
+              />
+              <ButtonBlock>
+                <CardButton type="submit">{t("common:send")}</CardButton>
+              </ButtonBlock>
+            </PageForm>
+          </FormControl>
+        </FormBody>
+      </BodyForm>
+    </>
+  );
+};
 
 export default BuyPolis;
--- components/personal-area/polic-updates/pesonal-updates.tsx
+++ components/personal-area/polic-updates/pesonal-updates.tsx
@@ -1,80 +1,191 @@
-import { FC } from "react";
-import NextLink from "next/link";
-import {PersonalUdatesProps} from './personal-updates.t';
-import { useState } from "react";
+import { FC, useEffect, useContext, useState, forwardRef } from "react";
+import { PersonalUdatesProps } from "./personal-updates.t";
 import {
-        BodyForm,
-        FormBody,
-        FormHeading,
-        PageForm,
-        UserInfoInput,
-        UserNumber,
-        UserEmail,
-        ButtonAdd,
-        FormMessage,
-        ButtonBlock
-        
-        } from '../personal-buy/pesonal-buy.e'
-import {UptadeSelect} from './pesonal-.updates.e'
+  BodyForm,
+  FormBody,
+  FormHeading,
+  PageForm,
+  UserInfoInput,
+  UserNumber,
+  UserEmail,
+  ButtonAdd,
+  FormMessage,
+  ButtonBlock,
+} from "../personal-buy/pesonal-buy.e";
+import { useRouter } from "next/router";
+import { UptadeSelect } from "./pesonal-.updates.e";
 import { CardButton } from "../../yur-face-page/CardBlock/CardBlock.e";
 import { useTranslation } from "react-i18next";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import Router from "next/router";
+import { api } from "../../../services/api";
+import { AuthContext } from "../../../context/AuthContext";
+
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const PersonalUdates: FC<PersonalUdatesProps> = () => {
-    const {t} = useTranslation()
-    const [value, setValue] = useState('')
-    return (
-        <>
-        <BodyForm>
-            <FormBody>
-                <FormHeading>{t('common:Insurance_renewal_request')}</FormHeading>
-                <PageForm>
-                <UptadeSelect onChange={(e) => {setValue(e.target.value)}}>
-                        <option value="КАСКО">{t('commion:CASCO')}</option>
-                        <option selected value="b">{t('commion:CASCO')}</option>
-                        <option value="КАСКО">{t('commion:CASCO')}</option>
-                        <option value="КАСКО">{t('commion:CASCO')}</option>
-                </UptadeSelect>
-                <p>
-                    <UserInfoInput 
-                          id="demo-helper-text-misaligned"
-                          placeholder="Иванов Иван Иванович"
-                          label={t('common:name_data')}
-                    />
-                </p>
-                    <UserNumber 
-                               
-                                id="demo-helper-text-misaligned"
-                                label={t('common:Insurance_policy_series_and_umber')}
-                                />
-                    {/* <FormMessage placeholder="Сообщение"/> */}
-                    <p>
-                        <UserNumber
-                            className='myInput'
-                            placeholder="998 90 989-89-89"
-                            id="demo-helper-text-misaligned"
-                            label={t('common:Phone_number')}
-                        />
-                        <UserEmail 
-                              placeholder="status585@mail.ru"
-                              id="demo-helper-text-misaligned"
-                              label="Email"
-                        />
-                    </p>
-                    <FormMessage placeholder={t('common:Message')}/>
-                    <ButtonBlock>
-                        <CardButton>{t('common:send')}</CardButton>
-                    </ButtonBlock>
-                </PageForm>
-            </FormBody>
-        </BodyForm>
-     
+  const {
+    signIn,
+    registration,
+    setOpen,
+    open,
+    alert,
+    setAlert,
+    errorMsg,
+    setErrorMsg,
+  } = useContext(AuthContext);
+  const { t } = useTranslation();
+  const [value, setValue] = useState("");
+  const [polis, setPolis] = useState<any>({});
+  const [insurance, setInsurance] = useState<any>([]);
+  const handleChange = (event: any, field: any) => {
+    setPolis((prevState: any) => ({
+      ...prevState,
+      [field]: event.target.value,
+    }));
+  };
 
+  const router = useRouter();
 
-        </>
+  useEffect(() => {
+    api.get("cabinet/insurance/all").then((res) => {
+      setInsurance(res.data.data);
+    });
+  }, []);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    api
+      .post("cabinet/polis/post", polis)
+      .then((res) => {
+        console.log("object ==>", res);
+        setOpen(true);
+        setPolis({});
+        Router.push("/personal-area/polic-updates");
+      })
+      .catch((err) => {
+        // console.log("object ==>", err.response.data);
+        setAlert(true);
+        // console.log(err.response.data);
+        setErrorMsg({
+          type: "polis",
+          message: err.response.data,
+        });
+      });
+  };
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-    )
+    setOpen(false);
+    setAlert(false);
+    setErrorMsg(null);
+  };
+  return (
+    <>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
 
-}
+      <Snackbar open={alert} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          {/* @ts-ignore */}
+          {/* {errorMsg && errorMsg.type == "polis" && errorMsg.message} */}
 
-
+          {errorMsg &&
+            errorMsg.type == "polis" &&
+            // @ts-ignore
+            errorMsg.message.map((item, idx) => {
+              <div key={idx}>{item}</div>;
+            })}
+        </Alert>
+      </Snackbar>
+      <BodyForm>
+        <FormBody>
+          <FormHeading>{t("common:Insurance_renewal_request")}</FormHeading>
+          <PageForm onSubmit={handleSubmit}>
+            <UptadeSelect
+              onChange={(e) => handleChange(e, "insurance_id")}
+              name="insurance_id"
+              id="insurance_id"
+              value={polis.insurance_id}
+            >
+              <option selected>choos</option>
+              {insurance.map((item: any, idx: any) => (
+                <option value={item.id} key={idx}>
+                  {item.title}
+                </option>
+              ))}
+            </UptadeSelect>
+            <p>
+              <UserInfoInput
+                placeholder="Иванов Иван Иванович"
+                label={t("common:name_data")}
+                onChange={(e) => handleChange(e, "full_name")}
+                name="full_name"
+                value={insurance.full_name}
+                required
+                id="full_name"
+              />
+            </p>
+            <UserNumber
+              id="polis_seriya_number"
+              label={t("common:Insurance_policy_series_and_umber")}
+              onChange={(e) => handleChange(e, "polis_seriya_number")}
+              name="polis_seriya_number"
+              value={insurance.polis_seriya_number}
+              required
+            />
+            {/* <FormMessage placeholder="Сообщение"/> */}
+            <p>
+              <UserNumber
+                className="phonenumber1"
+                placeholder="998 90 989-89-89"
+                id="demo-helper-text-misaligned"
+                label={t("common:Phone_number")}
+                onChange={(e) => handleChange(e, "phonenumber1")}
+                name="phonenumber1"
+                required
+                value={insurance.phonenumber1}
+              />
+              <UserEmail
+                placeholder="email1"
+                id="demo-helper-text-misaligned"
+                label="Email"
+                onChange={(e) => handleChange(e, "email1")}
+                name="email1"
+                required
+                value={insurance.email1}
+              />
+            </p>
+            <FormMessage
+              placeholder={t("common:Message")}
+              onChange={(e) => handleChange(e, "text")}
+              name="text"
+              required
+              value={insurance.text}
+            />
+            <ButtonBlock>
+              <CardButton type="submit">{t("common:send")}</CardButton>
+            </ButtonBlock>
+          </PageForm>
+        </FormBody>
+      </BodyForm>
+    </>
+  );
+};
 
 export default PersonalUdates;
