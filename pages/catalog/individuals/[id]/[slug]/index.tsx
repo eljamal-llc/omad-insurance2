44d631096bb1675662174d2bda6916eb21f8a708
Nizomiddin Zaripov <38242828+programm011@users.noreply.github.com>
2022-02-25 11:49:36 +0500

    fix

--- pages/catalog/individuals/[id]/[slug]/index.tsx
+++ pages/catalog/individuals/[id]/[slug]/index.tsx
@@ -20,21 +20,21 @@ import { api } from "../../../../../services/api";
 import { INewsData } from "../../../../../components/common/news/news.t";
 import BreadcrumbsBlock from "../../../../../components/common/bread-crumbs/Breadcrumbs";
 
-export async function getStaticProps({ locale}: { locale: string }) {
+
+
+export const getStaticPaths: GetStaticPaths<{}> = async () => {
+  return {
+      paths: [
+      ], //indicates that no page needs be created at build time
+      fallback: 'blocking' //indicates the type of fallback
+  }
+}
+export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
     },
   };
-}
-
-export const getStaticPaths: GetStaticPaths<{ id: string, slug: string }> = async () => {
-  return {
-      paths: [
-   
-      ], //indicates that no page needs be created at build time
-      fallback: 'blocking' //indicates the type of fallback
-  }
 }
 
 export interface PartnerProps {}
